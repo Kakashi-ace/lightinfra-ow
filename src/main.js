3b296eea0be4586cae1c4b358e52bdfb732378b8
chenban <19921870014@163.com>
2026-08-17 18:22:44 +0800

    chore(deps): 引入 ant-design-vue 支撑导航下拉菜单

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import { Dropdown, Menu } from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(i18n)
+app.use(Dropdown)
+app.use(Menu)
 app.mount('#app')
